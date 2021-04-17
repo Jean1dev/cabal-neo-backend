a6b52c3a607c7b867ce26213d7689331abed5ec2
Jean <jeanlucafp@unesc.net>
2021-04-17 16:49:28 -0300

    files

---
+++ cabal-neo-admin/src/main/java/com/cabal/neio/CabalNeoAdminApplication.java
@@ -0,0 +1,13 @@
+package com.cabal.neio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabalNeoAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabalNeoAdminApplication.class, args);
+	}
+
+}
---
+++ cabal-neo-admin/src/test/java/com/cabal/neio/CabalNeoAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabal.neio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabalNeoAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
